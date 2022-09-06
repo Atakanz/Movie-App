ead59d96885d0272a3217d1b5695f808d1aef53d
Atakanz <atakanzer@gmail.com>
2022-09-06 04:09:46 +0300

    Button component added.

---
+++ BirFilm/src/Components/Buttons/Buttons.style.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    borderRadius: 10,
+    marginTop: 20,
+    backgroundColor: '#002b5b',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginHorizontal: 3,
+  },
+  text: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
