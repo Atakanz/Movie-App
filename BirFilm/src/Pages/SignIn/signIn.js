b703c4d1b7e4d05ddbb11947d408463cc023416c
Atakanz <atakanzer@gmail.com>
2022-09-06 04:11:25 +0300

    Login form component placed in loginpages.

--- BirFilm/src/Pages/SignIn/signIn.js
+++ BirFilm/src/Pages/SignIn/signIn.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import {SafeAreaView, Text, Button} from 'react-native';
 import styles from './signIn.style';
+import LoginForm from '../../Components/LoginForm/LoginForm';
 
 const SignIn = ({navigation}) => {
+  const signInButton = () => {
+    navigation.navigate('BottomTab');
+  };
+  const signUpButton = () => {
+    navigation.navigate('SignUp');
+  };
   return (
-    <SafeAreaView>
-      <Text>Hello SıgnIn!</Text>
-      <Button title="Go" onPress={() => navigation.navigate('SignUp')} />
+    <SafeAreaView style={styles.container}>
+      <LoginForm
+        isLogoExist={require('../../Assets/logo.png')}
+        // logo is shown only loginpages not editing page
+        holder1="E-mail"
+        holder2="Password"
+        name1="Sign In"
+        name2="Sign Up"
+        task1={signInButton}
+        task2={signUpButton}
+      />
     </SafeAreaView>
   );
 };
--- BirFilm/src/Pages/SignUp/signUp.js
+++ BirFilm/src/Pages/SignUp/signUp.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {SafeAreaView, Text, Button} from 'react-native';
 import styles from './signUp.style';
+import LoginForm from '../../Components/LoginForm/LoginForm';
 
 const SignUp = ({navigation}) => {
+  const goBack = () => {
+    return navigation.goBack();
+  };
   return (
-    <SafeAreaView>
-      <Text>Hello SıgnUp!</Text>
-      <Button title="go" onPress={() => navigation.navigate('BottomTab')} />
+    <SafeAreaView style={styles.container}>
+      <LoginForm
+        isLogoExist={require('../../Assets/logo.png')}
+        // logo is shown only loginpages not editing page
+        holder1="E-mail"
+        holder2="Password"
+        holder3="Password again"
+        name1="Sıgn Up"
+        task1={goBack}
+      />
     </SafeAreaView>
   );
 };
