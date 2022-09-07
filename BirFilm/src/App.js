2275870b9a6ccf2bf4c43b130398372bc4adc013
Atakanz <atakanzer@gmail.com>
2022-09-07 03:27:56 +0300

    User info redux added.

--- BirFilm/src/App.js
+++ BirFilm/src/App.js
@@ -4,23 +4,27 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignUp from './Pages/SignUp/signUp';
 import SignIn from './Pages/SignIn/signIn';
 import {BottomTab} from './Navigation/BottomTab';
-import MovieDetail from './Pages/MovieDetail/MovieDetail';
+import MovieDetail from './Pages/MovieDetail/movieDetail';
+import {store} from './Management/store';
+import {Provider} from 'react-redux';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="BottomTab" component={BottomTab} />
-        <Stack.Screen name="MovieDetail" component={MovieDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="BottomTab" component={BottomTab} />
+          <Stack.Screen name="MovieDetail" component={MovieDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
