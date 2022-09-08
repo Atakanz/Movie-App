99b1eaa169425d24e585e6fc1521bde838137110
Atakanz <atakanzer@gmail.com>
2022-09-08 21:50:24 +0300

    Loading added.

---
+++ BirFilm/src/Components/Loading/loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AnimatedLottieView from 'lottie-react-native';
+
+function Loading() {
+  return (
+    <AnimatedLottieView
+      source={require('../../Assets/loading.json')}
+      autoPlay
+    />
+  );
+}
+
+export default Loading;
