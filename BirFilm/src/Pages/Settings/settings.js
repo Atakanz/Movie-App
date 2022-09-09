6df7c372c667f29c1bca782f434758e6ad0e31a3
Atakanz <atakanzer@gmail.com>
2022-09-09 15:51:55 +0300

    Setting page added.

--- BirFilm/src/Pages/Settings/settings.js
+++ BirFilm/src/Pages/Settings/settings.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import {SafeAreaView, Text, Button} from 'react-native';
+import {SafeAreaView, Text, View, Image} from 'react-native';
 import styles from './settings.style';
+import Buttons from '../../Components/Buttons';
+import {useSelector} from 'react-redux';
 
 const Settings = ({navigation}) => {
+  const userInfo = useSelector(state => state.user.user);
+
   return (
-    <SafeAreaView>
-      <Text>Hello Settings!</Text>
-      <Button
-        title="Go"
-        onPress={() => {
-          navigation.navigate('EditProfile');
-        }}
-      />
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={styles.profilePhotoView}>
+          <Image
+            source={require('../../Assets/atakan.jpg')}
+            style={styles.profilePhoto}
+          />
+          <View style={styles.profileInfoCard}>
+            <Text style={styles.profileName}>E-mail: {userInfo.email}</Text>
+
+            <Text style={styles.profileName}>User: {userInfo.username}</Text>
+          </View>
+        </View>
+        <View style={styles.button}>
+          <Buttons
+            name="Theme Settings"
+            task={() => navigation.navigate('ThemeSettings')}
+          />
+          <Buttons
+            name="Edit Profile"
+            task={() => navigation.navigate('EditProfile')}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
