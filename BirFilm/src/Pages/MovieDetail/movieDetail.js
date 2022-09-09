8535a29fa9d5bac42e1bcb91ec96572cb7e86609
Atakanz <atakanzer@gmail.com>
2022-09-09 12:53:17 +0300

    Movie detail page added.

--- BirFilm/src/Pages/MovieDetail/movieDetail.js
+++ BirFilm/src/Pages/MovieDetail/movieDetail.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, View, Image, ScrollView} from 'react-native';
 import styles from './movieDetail.style';
+import axios from 'axios';
 
-const MovieDetail = () => {
+const MovieDetail = ({route, navigation}) => {
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.themoviedb.org/3/genre/movie/list?api_key=330045487adad93254480466da226e81',
+      )
+      .then(response => {
+        setCategory(response.data.genres);
+      });
+  }, []);
+  const {item} = route.params;
+  const genreNames = category.filter(genre => genre.id === item.genre_ids);
+  const onlyGenreNames = genreNames.map(item => item.name);
   return (
-    <SafeAreaView>
-      <Text>Hello MovieDetail!</Text>
-    </SafeAreaView>
+    <ScrollView>
+      <View style={styles.coverPhotoView}>
+        <Image
+          style={styles.coverPhoto}
+          source={{uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`}}
+        />
+      </View>
+      <View style={styles.coverPhotoView}>
+        <Text style={styles.textTitle}>{item.title}</Text>
+      </View>
+      <View style={styles.coverPhotoView}>
+        <Text style={styles.textOverview}>{item.overview}</Text>
+      </View>
+      <View style={styles.coverPhotoView}>
+        <Text style={styles.textOverview}>{onlyGenreNames.join(',')}</Text>
+      </View>
+    </ScrollView>
   );
 };
 
--- BirFilm/src/Pages/MovieDetail/movieDetail.style.js
+++ BirFilm/src/Pages/MovieDetail/movieDetail.style.js
@@ -1,3 +1,28 @@
-import {StyleSheet} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  coverPhoto: {
+    width: '100%',
+    height: Dimensions.get('window').height / 1.2,
+    resizeMode: 'contain',
+  },
+  textTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingLeft: 10,
+    paddingTop: 10,
+    color: 'darkblue',
+  },
+  textOverview: {
+    fontSize: 16,
+    paddingLeft: 10,
+    paddingTop: 20,
+    color: 'gray',
+    maxWidth: 400,
+    textAlign: 'justify',
+    fontWeight: 'bold',
+  },
+});
