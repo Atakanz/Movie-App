fb99d83bf50edf164cf786b11e1e84ef9285b61d
Atakanz <atakanzer@gmail.com>
2022-09-10 02:56:41 +0300

    Theme settings page added.

--- BirFilm/src/Pages/ThemeSettings/themeSettings.js
+++ BirFilm/src/Pages/ThemeSettings/themeSettings.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, TouchableOpacity, Text, View} from 'react-native';
 import styles from './themeSettings.style';
+import themeSlice from '../../Management/Features/Theme/themeSlice';
+import {useDispatch, useSelector} from 'react-redux';
+import {setTheme} from '../../Management/Features/Theme/themeSlice';
 
-const ThemeSettings = () => {
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+const ThemeSettings = ({navigation}) => {
+  const dispatch = useDispatch();
+  const theme = useSelector(state => state.theme.theme);
+  const themeToggle = () => {
+    if (theme === 'Light') {
+      dispatch(setTheme('Dark'));
+    } else {
+      dispatch(setTheme('Light'));
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Hello themeSettings!</Text>
+    <SafeAreaView style={[styles.container, styles[`container${theme}`]]}>
+      <View style={styles.touchableView}>
+        <TouchableOpacity
+          style={[styles.button, styles[`button${theme}`]]}
+          onPress={themeToggle}>
+          <Text style={[styles.text, styles[`text${theme}`]]}>{theme}</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
