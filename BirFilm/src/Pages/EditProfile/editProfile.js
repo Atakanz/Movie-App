beb201c60ebdae1a2b0d970f58e4e6bf322ab489
Atakanz <atakanzer@gmail.com>
2022-09-09 15:51:23 +0300

    Login form added.

--- BirFilm/src/Pages/EditProfile/editProfile.js
+++ BirFilm/src/Pages/EditProfile/editProfile.js
@@ -1,30 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, Text, Button} from 'react-native';
 import styles from './editProfile.style';
 import {useSelector, useDispatch} from 'react-redux';
 import {setUser} from '../../Management/Features/Login/userSlice';
+import {setAuth} from '../../Management/Features/Auth/authSlice';
+import {setLoading} from '../../Management/Features/Loading/loadingSlice';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import LoginForm from '../../Components/LoginForm/LoginForm';
 
 const EditProfile = ({navigation}) => {
+  const [userEmail, setUserEmail] = useState(null);
+  const [userPassword, setUserPassword] = useState(null);
+  const [userName, setUserName] = useState(null);
   const dispatch = useDispatch();
   const remover = async () => {
     dispatch(setUser(null));
     await AsyncStorage.removeItem('savedItem');
-    navigation.navigate('SignIn');
+    dispatch(setAuth(false));
   };
-  const removeStatus = async () => {
-    await AsyncStorage.setItem('savedLoginStatus', JSON.stringify(false));
-  };
-  const userInfo = useSelector(state => state.user.user);
+
   const logOut = () => {
     remover();
     // console.log(userInfo);
   };
+
+  const changeInformation = () => {
+    dispatch(
+      setUser({
+        email: userEmail,
+        password: userPassword,
+        username: userName,
+      }),
+    );
+    AsyncStorage.setItem(
+      'savedItem',
+      JSON.stringify({
+        email: userEmail,
+        password: userPassword,
+        username: userName,
+      }),
+    );
+  };
   return (
-    <SafeAreaView>
-      {/* <Text>{userRedux}</Text> */}
-      <Text>Hello editProfile!</Text>
-      <Button onPress={logOut} title="Log out" />
+    <SafeAreaView style={styles.enabledDirection}>
+      <LoginForm
+        isLogoExist={require('../../Assets/logo.png')}
+        holder1="E-mail"
+        holder2="Password"
+        holder4="User name"
+        name1="Change information"
+        name2="Log Out"
+        value1={userEmail}
+        value2={userPassword}
+        value4={userName}
+        emailFormTask={value => setUserEmail(value)}
+        passwordFormTask={value => setUserPassword(value)}
+        userNameFormTask={value => setUserName(value)}
+        task1={changeInformation}
+        task2={logOut}
+        visibilityFalse={false}
+        visibilityTrue={true}
+      />
     </SafeAreaView>
   );
 };
