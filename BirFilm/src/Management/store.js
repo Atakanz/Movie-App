3dd07828860ef4fefd4871e71913c71c0e2a50ee
Atakanz <atakanzer@gmail.com>
2022-09-10 03:04:40 +0300

    Theme slice added.

--- BirFilm/src/Management/store.js
+++ BirFilm/src/Management/store.js
@@ -3,11 +3,13 @@ import authSlice from './Features/Auth/authSlice';
 import userSlice from './Features/Login/userSlice';
 import loadingSlice from './Features/Loading/loadingSlice';
 import movieListSlice from './Features/MovieList/movieListSlice';
+import themeSlice from './Features/Theme/themeSlice';
 export const store = configureStore({
   reducer: {
     user: userSlice,
     auth: authSlice,
     loading: loadingSlice,
     movieList: movieListSlice,
+    theme: themeSlice,
   },
 });
